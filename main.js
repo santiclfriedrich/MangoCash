0851d8be0b95f5bd254476fdd85ee60a620b97cb
GabiiMollo <jgabimollo@gmail.com>
2024-09-12 00:27:03 -0300

    login y registro

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
 import './style.css'
+
+// Abre el modal
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = 'flex';
+    document.querySelector('body').style.overflow = 'hidden';
+}
+  
+// Cierra el modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+    document.querySelector('body').style.overflowY = 'auto';
+}
+
+// Cierra el modal si se hace clic fuera de él
+window.onclick = function(event) {
+    const loginModal = document.getElementById('loginModal');
+    const registroModal = document.getElementById('registroModal');
+
+    if (event.target === loginModal) {
+        closeModal('loginModal');
+    }
+
+    if (event.target === registroModal) {
+        closeModal('registroModal');
+    }
+}
+
+document.getElementsByClassName('--signin')[0].addEventListener('click', () => {
+    openModal('loginModal');
+});
+
+document.getElementsByClassName('--signup')[0].addEventListener('click', () => {
+    openModal('registroModal');
+});
+
+document.getElementsByClassName('closeModalLogin')[0].addEventListener('click', () => {
+    closeModal('loginModal');
+});
+
+document.getElementsByClassName('closeModalRegistro')[0].addEventListener('click', () => {
+    closeModal('registroModal');
+});
+  
+  
